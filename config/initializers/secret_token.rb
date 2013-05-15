3b2ab9e8e67089cdab797892660a376187ce273b
Hakim Benbekhti <hakim.benbekhti@gmail.com>
2013-05-15 19:38:14 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MjDeploy::Application.config.secret_token = '15fe7ef7a5395974e3fd27cac4b9a0f9ef320922c6efd21813f05b04aae292befc123b0506ecf9591250ad33a34cad6bc60521ce700b0f40ffdafb2aeb238394'
